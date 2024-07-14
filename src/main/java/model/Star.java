9adfee43a8b5dcf1a18600d47c9e449259b954eb
Paweł Grabowski <p.grabowski@ultimate.systems>
2024-07-14 18:00:42 +0200

    Added Spring

---
+++ src/main/java/model/Star.java
@@ -0,0 +1,12 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Star {
+    private String name;
+    private Long distance;
+
+}
---
+++ src/test/java/com/grabowski/MyPlan/MyPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabowski.MyPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
